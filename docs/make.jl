d44595980e4ade8cc82043a447ff0501511d98b8
Ben Elkin <8xe8n-cm@online.de>
2023-12-17 22:59:34 +0100

    adapt make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,6 @@
 using Documenter, Unitful, Dates
 
-ENV["UNITFUL_FANCY_EXPONENTS"] = false
-
-DocMeta.setdocmeta!(Unitful, :DocTestSetup, :(using Unitful))
+const ci = get(ENV, "CI", nothing) == "true"
 
 function check_defaultunits_version()
     vfile = "docs/src/assets/vfile.txt"
@@ -10,14 +8,23 @@ function check_defaultunits_version()
     docs_v = VersionNumber(r)
     pkg_v = pkgversion(Unitful)
     docs_v == pkg_v || error("Docs chapter on default units built with the wrong version of Unitful \
-    (docs built for $docs_v vs current Unitful version $pkg_v). \
-    Please run the script on the local computer with the proper Unitful version")
+        (docs built for $docs_v vs current Unitful version $pkg_v). \
+        Please run the script on the local computer with the proper Unitful version")
     return nothing
 end
 
+# on local computer, (re-)create the documentation file defaultunits.md
+if !ci
+    ENV["UNITFUL_FANCY_EXPONENTS"] = false
+    include("make_def-units_docs.jl")
+    MakeDefUnitsDocs.make_chapter()
+end
+
+DocMeta.setdocmeta!(Unitful, :DocTestSetup, :(using Unitful))
+
 makedocs(
     sitename = "Unitful.jl",
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    format = Documenter.HTML(prettyurls = ci),
     warnonly = [:missing_docs],
     modules = [Unitful],
     workdir = joinpath(@__DIR__, ".."),
@@ -39,4 +46,7 @@ makedocs(
     ]
 )
 
-# deploydocs(repo = "github.com/PainterQubits/Unitful.jl.git")
+if ci
+    check_defaultunits_version()
+    deploydocs(repo = "github.com/PainterQubits/Unitful.jl.git")
+end
